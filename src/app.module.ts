44f66c8891e8b7c2de6fcc1722a7eac33aaa7ff1
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2025-02-08 19:38:04 +0200

    deployment fix 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,7 @@ import { Superhero } from './superhero/superhero.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT as any, 10),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL,
       entities: [Superhero],
       synchronize: true,
     }),
